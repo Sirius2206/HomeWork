8071a7ab2e77e364fc94816b3694acefa36537a0
sirius2206 <sergei_galkin@inbox.ru>
2021-08-27 01:28:32 +0500

    Урок 2: Домашнее задание по циклам

--- src/main/java/Lesson1/Lesson1.java
+++ src/main/java/Lesson1/Lesson1.java
@@ -20,7 +20,7 @@ public class Lesson1 {
         int value = 99;
         if (value <= 0){
             System.out.println("Красный");
-        } else if ((value > 0) && (value <= 100)){
+        } else if (value <= 100){
             System.out.println("Желтый");
         } else{
             System.out.println("Зеленый");
